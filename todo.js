3d595969832607f912f62e6570ca02fda3aedcce
mareigh24 <ajayimary300@gmail.com>
2024-01-22 15:13:50 +0100

    completed

---
+++ todo.js
@@ -0,0 +1,28 @@
+function newElement() {
+  let myInput = document.getElementById("myInput");
+  let lists = document.getElementById("lists");
+
+  if (myInput.value !== "") {
+    let newTask = document.createElement("ul");
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", function () {
+      newTask.style.textDecoration = checkbox.checked ? "line-through" : "none";
+    });
+    let taskLabel = document.createElement("label");
+    taskLabel.textContent = myInput.value;
+
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "delete";
+
+    deleteButton.addEventListener("click", function () {
+      lists.removeChild(newTask);
+    });
+    newTask.appendChild(checkbox);
+    newTask.appendChild(taskLabel);
+    newTask.appendChild(deleteButton);
+
+    lists.appendChild(newTask);
+    myInput.value = "";
+  }
+}
